fab145dc1d705edc1ad8dfc0ede15e903ce8b04e
yhavry <34289148+yhavry@users.noreply.github.com>
2025-07-15 23:32:00 -0400

    Update midea_ir.h

--- esphome/components/midea_ir/midea_ir.h
+++ esphome/components/midea_ir/midea_ir.h
@@ -16,11 +16,11 @@ class MideaIR : public climate_ir::ClimateIR {
  public:
   MideaIR()
       : climate_ir::ClimateIR(
-            MIDEA_TEMPC_MIN, MIDEA_TEMPC_MAX, 1.0f, true, true,
-            {climate::CLIMATE_FAN_AUTO, climate::CLIMATE_FAN_LOW, climate::CLIMATE_FAN_MEDIUM,
-             climate::CLIMATE_FAN_HIGH},
-            {climate::CLIMATE_SWING_OFF, climate::CLIMATE_SWING_VERTICAL},
-            {climate::CLIMATE_PRESET_NONE, climate::CLIMATE_PRESET_SLEEP, climate::CLIMATE_PRESET_BOOST}) {}
+    MIDEA_TEMPC_MIN, MIDEA_TEMPC_MAX, 1.0f, true, false,  // ❌ disable HEAT
+    {climate::CLIMATE_FAN_AUTO, climate::CLIMATE_FAN_LOW, climate::CLIMATE_FAN_MEDIUM,
+     climate::CLIMATE_FAN_HIGH},
+    {},  // ❌ remove all swing modes
+    {climate::CLIMATE_PRESET_NONE, climate::CLIMATE_PRESET_SLEEP, climate::CLIMATE_PRESET_BOOST}) {}
 
   /// Override control to change settings of the climate device.
   void control(const climate::ClimateCall &call) override;
